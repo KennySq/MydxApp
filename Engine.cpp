eb3534927f532a1dd21b2246e7b9f37c0bcde79a
권필성 <odessa5d4@gmail.com>
2021-02-19 00:51:58 +0900

    wrong file removed

--- Engine.cpp
+++ Engine.cpp
@@ -19,13 +19,17 @@ void Engine::PreInit()
 
 	auto scDesc = CD3D11_TEXTURE2D_DESC(DXGI_FORMAT_R8G8B8A8_UNORM, mHardware->GetWidth(), mHardware->GetHeight(), 1U, 0U, D3D11_BIND_RENDER_TARGET);
 	auto depthDesc = CD3D11_TEXTURE2D_DESC(DXGI_FORMAT_R24G8_TYPELESS, mHardware->GetWidth(), mHardware->GetHeight(), 1U, 0U, D3D11_BIND_DEPTH_STENCIL);
-
+	auto cubeDesc = CD3D11_TEXTURE2D_DESC(DXGI_FORMAT_R32G32B32A32_FLOAT, 1024, 1024, 6U, 1U, D3D11_BIND_SHADER_RESOURCE, D3D11_USAGE_DEFAULT);
+	
+	
 	Tex2D* scTex = new Tex2D(scDesc, true);
 	Tex2D* depthTex = new Tex2D(depthDesc);
-
+///	Tex2D* cubeMap = new Tex2D(cubeDesc, false, true);
+	Tex2D* cubeMap = new Tex2D("resources/SampleCubemap.dds");
+	
 	r2d.AddTexture2D(scTex);
 	r2d.AddTexture2D(depthTex);
-
+	r2d.AddTexture2D(cubeMap);
 	scene->Init();
 }
 
@@ -34,26 +38,41 @@ void Engine::Init()
 	AllocConsole();
 
 	mSwapChain = HW::GetSwapChain();
+	Renderer2D& r2d = Renderer2D::GetInstance();
 
-	Pass* p = new Pass("C:/Users/Leopard/source/repos/Mydx/StandardForwardShader.hlsl", "Sample", VERTEX | PIXEL, FORWARD);
+	Pass* p = new Pass("C:/Users/Kenny/Documents/Mydx/StandardForwardShader.hlsl", "Sample", VERTEX | PIXEL, FORWARD);
 	Mesh* mesh = new Mesh(*PrimitiveGenerator::GenerateSphere(1.0f, 32, 32));
 	Camera* mainCam = scene->GetCamera();
 
 	Instance* inst = new Instance();
-	inst->AddComponent<MeshRenderer>();
-	Transform* transform = inst->GetTransform();
+	Instance* skybox = new Instance();
 
+	inst->AddComponent<MeshRenderer>();
+	skybox->AddComponent<MeshRenderer>();
+	
+	Transform* transform = inst->GetTransform();
+	Transform* skyTransform = skybox->GetTransform();
 
 	scene->AddInstance(inst);
+	scene->AddInstance(skybox);
+
 	scene->Init();
 	
-	MeshRenderer* mr = inst->GetComponent<MeshRenderer>();
 	
+
+	MeshRenderer* mr = inst->GetComponent<MeshRenderer>();
+	MeshRenderer* skyboxMr = skybox->GetComponent<MeshRenderer>();
+
 	mr->SetMesh(&*mesh);
 	mr->SetPass(p);
+	
+	skyboxMr->SetMesh(&*mesh);
+	mr->SetPass(p);
+	
 	RenderState& rs = mr->GetState();
+	RenderState& skyboxRs = skyboxMr->GetState();
 
-	XMVECTOR lightDir = XMVectorSet(-1, 0, 0, 1) - XMVectorSet(0, 0, 0, 0);
+	XMVECTOR lightDir = XMVectorSet(1, 0, 0, 1) - XMVectorSet(0, 0, 0, 0);
 	XMFLOAT4 lightDirf4;
 
 	XMStoreFloat4(&lightDirf4, lightDir);
@@ -67,6 +86,10 @@ void Engine::Init()
 	rs.AddResource(mainCam->GetBuffer(), PIXEL, 0);
 	rs.AddResource(transform->GetBuffer(), PIXEL, 1);
 	rs.AddResource(light->AsBuffer(), PIXEL, 2);
+
+	Tex2D* cubeTex = r2d.GetTexture2D(2);
+
+	rs.AddResource(cubeTex->GetShaderResource(), PIXEL, 0);
 	// Instance->GetBuffer();
 			
 
